a95fa3cea2c7addfcf559bbe41aa6371ccd788ad
JauniKapauni <riches-goods.54@icloud.com>
2026-05-20 16:23:54 +0200

    feat: add DatabaseManager

--- src/main/java/de/jaunikapauni/axperms/AxPerms.java
+++ src/main/java/de/jaunikapauni/axperms/AxPerms.java
@@ -1,12 +1,27 @@
 package de.jaunikapauni.axperms;
 
+import de.jaunikapauni.axperms.manager.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxPerms extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager(){
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        databaseManager = new DatabaseManager(this);
+        try{
+            if(databaseManager.initDatabaseTable1() == false){
+                Bukkit.getLogger().severe("Error creating db table!");
+                Bukkit.getServer().shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
